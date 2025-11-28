f4903ddb955b6961cb696fa8b8282c1977a2f005
binoypr07 <binoypr07@gmail.com>
2025-11-28 18:26:06 +0530

    upgrade

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,6 +19,7 @@ pluginManagement {
 
 plugins {
     id("com.android.application") version "8.3.2" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
